c08a2f7ea20381c18b2aab9b11f3f056c5e2e406
Maria Suelany <suelanybr@gmail.com>
2018-12-28 23:13:49 -0200

    Add Resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -49,10 +49,35 @@ class Resume extends Component {
                             <div className="card-body text-left mt-5 ml-5">
                                 <h5 className="card-title pt-5 text-dark">Maria Suelany Brito da Cruz</h5>
                                 <h6 className="card-subtitle mb-2 text-muted text-dark">Student</h6>
-                                <p className="mb-0 text-dark"><strong>Major: </strong> Undergraduate student in Computer Science (UFCG)</p>
+                                <p className="mb-0 text-dark"><strong>Major: </strong> Undergraduate student</p>
                                 <p className="mb-0 text-dark"><strong>Semester: </strong> 8° </p>
                                 <p className="mb-0 text-dark"><strong>Email</strong> Suelanybr@gmail.com</p>
-                                <p className="mt-4 pb-5 text-dark"><strong>Resume</strong></p>
+
+                                <div className="text-dark">
+                                    <h5 className="mt-4 text-dark"><strong>Education</strong></h5>
+                                    <p> Federal University of Campina Grande (UFCG) </p>
+                                    <p> Undergraduate student of Computer Science • 2014 - expected 2020 </p>
+                                    <p className="mt-4 text-dark"><strong>Experience</strong></p>
+                                    <p> Monitor (Jan 2015 - Aug 2016 and June 2018 - Nov 2018)</p>
+                                    <ul>
+                                        <li>Java</li>
+                                        <li>Designer patterns</li>
+                                        <li>TDD with JUnit</li>
+                                    </ul>
+                                </div>
+
+                                <div className="text-dark">
+                                    <h5 className="mt-4 text-dark"><strong>Acquirements</strong></h5>
+                                    <ul>
+                                        <li>Java</li>
+                                        <li>C++</li>
+                                        <li>Python</li>
+                                        <li>JavaScript</li>
+                                        <li>NodeJs</li>
+                                        <li>ReactJs</li>
+                                    </ul>
+                                </div>
+
                             </div>
                         </div>
                     </div>
